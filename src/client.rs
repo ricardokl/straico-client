0e19580cfc771c6e9a3d8943c8204420231eb0d6
Ricardo Kullock <ricardokullock@gmail.com>
2024-10-23 15:50:01 +0200

    refactor: Replace Endpoint with PostEndpoint and GetEndpoint in client.rs.

--- src/client.rs
+++ src/client.rs
@@ -8,7 +8,7 @@ use serde::Serialize;
 use crate::common::ApiResponse;
 use crate::endpoints::completion::{CompletionRequest, CompletionsData};
 use crate::endpoints::image::{ImageData, ImageRequest};
-use crate::{Endpoint, BASE_URL};
+use crate::{GetEndpoint, PostEndpoint, BASE_URL};
 
 pub struct Client<A> {
     client: ReqwestClient,
@@ -26,28 +26,28 @@ impl<A: fmt::Display> Client<A> {
         }
     }
 
-    pub async fn post<T, R>(&self, endpoint: &Endpoint, payload: &T) -> ApiResponse<R>
+    pub async fn post<T, R>(&self, endpoint: &PostEndpoint, payload: &T) -> ApiResponse<R>
     where
-        T: Serialize +?Sized,
+        T: Serialize + ?Sized,
         R: DeserializeOwned,
     {
         let url = Url::parse(BASE_URL)?.join(endpoint.as_ref())?;
         let response = self
-           .client
-           .post(url)
-           .bearer_auth(&self.api_key)
-           .json(&payload)
-           .send()
-           .await?;
+            .client
+            .post(url)
+            .bearer_auth(&self.api_key)
+            .json(&payload)
+            .send()
+            .await?;
 
         Ok(response.json().await?)
     }
 
     pub async fn create_image(&self, request: &ImageRequest) -> ApiResponse<ImageData> {
-        self.post(&Endpoint::Image, request).await
+        self.post(&PostEndpoint::Image, request).await
     }
 
     pub async fn completion(&self, request: &CompletionRequest) -> ApiResponse<CompletionsData> {
-        self.post(&Endpoint::Completion, request).await
+        self.post(&PostEndpoint::Completion, request).await
     }
 }
