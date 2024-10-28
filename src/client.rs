b32c650fc1ca9ceabce96ff5c82f5e61495016e5
Ricardo Kullock <ricardokullock@gmail.com>
2024-10-28 09:51:38 +0100

    moving to macros

--- src/client.rs
+++ src/client.rs
@@ -1,120 +1,137 @@
-use core::fmt::{Display, Formatter, Result};
 use reqwest::Client as ReqwestClient;
+use reqwest::RequestBuilder as ReqwestBuilder;
 use serde::{de::DeserializeOwned, Serialize};
+use std::fmt::Display;
+use std::marker::PhantomData;
 
 use crate::common::ApiResponse;
-use crate::endpoints::{
-    audio::{TranscriptionRequest, TranscriptionData},
-    completion::{CompletionRequest, CompletionsData},
-    embeddings::{EmbeddingsRequest, EmbeddingsData},
-    file::FileData,
-    image::{ImageData, ImageRequest},
-    model::ModelData,
-    user::UserData,
-};
+use crate::endpoints::{completion::CompletionRequest, image::ImageRequest};
 use crate::{GetEndpoint, PostEndpoint};
 
+pub struct ApiKeyNotSet;
+pub struct ApiKeySet;
+pub struct PayloadSet;
+pub struct UserNoPayload;
+pub struct ModelsNoPayload;
+
+pub struct StraicoRequestBuilder<State, T = ()> {
+    client: ReqwestBuilder,
+    _payload: PhantomData<T>,
+    _state: PhantomData<State>,
+}
+
 pub trait IntoStraicoClient {
-    fn to_straico(self, api_key: String) -> StraicoClient;
+    fn to_straico(self) -> StraicoClient;
 }
 
 impl IntoStraicoClient for ReqwestClient {
-    fn to_straico(self, api_key: String) -> StraicoClient {
-        StraicoClient {
-            client: self,
-            api_key,
-        }
+    fn to_straico(self) -> StraicoClient {
+        StraicoClient { client: self }
     }
 }
 
 pub struct StraicoClient {
     client: ReqwestClient,
-    api_key: String,
 }
 
-impl StraicoClient {
-    pub fn new(api_key: String) -> Self {
-        Self {
-            client: ReqwestClient::new(),
-            api_key,
+// impl StraicoClient {
+//     pub fn file(self) -> StraicoRequestBuilder<ApiKeyNotSet, ImageRequest> {
+//         StraicoRequestBuilder::<ApiKeyNotSet, ImageRequest> {
+//             client: self.client.post(PostEndpoint::File.as_ref()),
+//             _payload: PhantomData,
+//             _state: PhantomData,
+//         }
+//     }
+
+//     pub fn create_image(&self) -> StraicoRequestBuilder<ApiKeyNotSet, ImageRequest> {
+//         StraicoRequestBuilder::<ApiKeyNotSet, ImageRequest> {
+//             client: self.client.post(PostEndpoint::Image.as_ref()),
+//             _payload: PhantomData,
+//             _state: PhantomData,
+//         }
+//     }
+
+//     pub fn completion(&self) -> StraicoRequestBuilder<ApiKeyNotSet, CompletionRequest> {
+//         StraicoRequestBuilder::<ApiKeyNotSet, CompletionRequest> {
+//             client: self.client.post(PostEndpoint::Completion.as_ref()),
+//             _payload: PhantomData,
+//             _state: PhantomData,
+//         }
+//     }
+
+//     pub fn user(&self) -> StraicoRequestBuilder<ApiKeyNotSet> {
+//         StraicoRequestBuilder::<ApiKeyNotSet> {
+//             client: self.client.get(GetEndpoint::User.as_ref()),
+//             _payload: PhantomData,
+//             _state: PhantomData,
+//         }
+//     }
+
+//     pub fn models(&self) -> StraicoRequestBuilder<ApiKeyNotSet> {
+//         StraicoRequestBuilder::<ApiKeyNotSet> {
+//             client: self.client.get(GetEndpoint::Models.as_ref()),
+//             _payload: PhantomData,
+//             _state: PhantomData,
+//         }
+//     }
+// }
+
+impl<T> StraicoRequestBuilder<ApiKeyNotSet, T> {
+    pub fn set_key<K: Display>(self, api_key: K) -> StraicoRequestBuilder<ApiKeySet, T> {
+        StraicoRequestBuilder::<ApiKeySet, T> {
+            client: self.client.bearer_auth(api_key),
+            _payload: PhantomData,
+            _state: PhantomData,
         }
     }
+}
 
-    pub async fn post<T, R>(&self, endpoint: &PostEndpoint, payload: &T) -> ApiResponse<R>
-    where
-        T: Serialize +?Sized,
-        R: DeserializeOwned,
-    {
-        let response = self
-           .client
-           .post(endpoint.as_ref())
-           .bearer_auth(&self.api_key)
-           .json(&payload)
-           .send()
-           .await?;
+impl StraicoRequestBuilder<ApiKeySet> {
+    pub async fn send<R: DeserializeOwned>(self) -> ApiResponse<R> {
+        let response = self.client.send().await?;
 
         Ok(response.json().await?)
     }
+}
 
-    pub async fn get<R>(&self, endpoint: &GetEndpoint) -> ApiResponse<R>
-    where
-        R: DeserializeOwned,
-    {
-        let response = self
-           .client
-           .get(endpoint.as_ref())
-           .bearer_auth(&self.api_key)
-           .send()
-           .await?;
+impl<T: Serialize> StraicoRequestBuilder<ApiKeySet, T> {
+    pub fn with_payload(self, payload: &T) -> StraicoRequestBuilder<PayloadSet, T> {
+        StraicoRequestBuilder::<PayloadSet, T> {
+            client: self.client.json(payload),
+            _payload: PhantomData,
+            _state: PhantomData,
+        }
+    }
+}
+
+impl<T: Serialize> StraicoRequestBuilder<PayloadSet, T> {
+    pub async fn send<R: DeserializeOwned>(self) -> ApiResponse<R> {
+        let response = self.client.send().await?;
 
         Ok(response.json().await?)
     }
+}
 
-    pub async fn create_image(&self, request: &ImageRequest) -> ApiResponse<ImageData> {
-        self.post(&PostEndpoint::Image, request).await
-    }
+macro_rules! define_endpoints {
+    ($($ident:ident ($name:ident, $endpoint:path, $payload:ty)),* $(,)?) => {
+        impl StraicoClient {
+            $(
+                pub fn $name(&self) -> StraicoRequestBuilder<ApiKeyNotSet, $payload> {
+                    StraicoRequestBuilder::<ApiKeyNotSet, $payload> {
+                        client: self.client.$ident($endpoint.as_ref()),
+                        _payload: PhantomData,
+                        _state: PhantomData,
+                    }
+                }
+            )*
+        }
+    };
+}
 
-    pub async fn completion(&self, request: &CompletionRequest) -> ApiResponse<CompletionsData> {
-        self.post(&PostEndpoint::Completion, request).await
-    }
-
-    pub async fn user(&self) -> ApiResponse<UserData> {
-        self.get(&GetEndpoint::User).await
-    }
-
-    pub async fn models(&self) -> ApiResponse<ModelData> {
-        self.get(&GetEndpoint::Models).await
-    }
-
-    pub async fn post_multipart<R>(&self, endpoint: &PostEndpoint, form: Form) -> ApiResponse<R>
-    where
-        R: DeserializeOwned,
-    {
-        let response = self
-            .client
-            .post(endpoint.as_ref())
-            .bearer_auth(&self.api_key)
-            .multipart(form)
-            .send()
-            .await?;
-
-        Ok(response.json().await?)
-    }
-
-    pub async fn upload_file(&self, request: &FileUploadRequest) -> ApiResponse<FileData> {
-        let form = request.to_form().await?;
-        self.post_multipart(&PostEndpoint::File, form).await
-    }
-
-    pub async fn transcribe(&self, request: &TranscriptionRequest) -> ApiResponse<TranscriptionData> {
-        self.post(&PostEndpoint::AudioTranscription, request).await
-    }
-
-    pub async fn generate_audio(&self, request: &GenerationRequest) -> ApiResponse<GenerationData> {
-        self.post(&PostEndpoint::AudioGeneration, request).await
-    }
-
-    pub async fn embeddings(&self, request: &EmbeddingsRequest) -> ApiResponse<EmbeddingsData> {
-        self.post(&PostEndpoint::Embeddings, request).await
-    }
+define_endpoints! {
+    post(file, PostEndpoint::File, ImageRequest),
+    post(create_image, PostEndpoint::Image, ImageRequest),
+    post(completion, PostEndpoint::Completion, CompletionRequest),
+    get(user, GetEndpoint::User, UserNoPayload),
+    get(models, GetEndpoint::Models, ModelsNoPayload),
 }
