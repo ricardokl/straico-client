db379026363286068ff85950b8e7d9828ac9693c
Ricardo Kullock <ricardokullock@gmail.com>
2024-11-19 15:09:21 +0100

    refactor: Remove unused actix-web error conversion implementation

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,4 @@
 use crate::endpoints::ApiResponseData;
-use actix_web::error::InternalError;
-use actix_web::Error;
 use reqwest::Error as ReqwestError;
 use serde_json::Error as SerdeError;
 
@@ -21,19 +19,5 @@ impl From<ReqwestError> for ApiError {
 impl From<SerdeError> for ApiError {
     fn from(error: SerdeError) -> Self {
         ApiError::Parsing(error)
-    }
-}
-
-impl From<ApiError> for actix_web::Error {
-    fn from(error: ApiError) -> Self {
-        match error {
-            // ApiError::Network(error) => actix_web::Error::from(error),
-            ApiError::Parsing(error) => Error::from(error),
-            _ => InternalError::new(
-                "Reqwest Error",
-                actix_web::http::StatusCode::INTERNAL_SERVER_ERROR,
-            )
-            .into(),
-        }
     }
 }
