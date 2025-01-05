9bb8c54a2641875fb22af3e574c10cd1da64464a
Ricardo Kullock <ricardokullock@gmail.com>
2025-01-05 17:06:41 +0100

    simpler file.rs

--- src/endpoints/file.rs
+++ src/endpoints/file.rs
@@ -1,34 +1,8 @@
 use serde::Deserialize;
-use std::path::Path;
 
 #[derive(Debug, Deserialize)]
 pub struct FileData {
     pub url: String,
 }
 
-#[allow(dead_code)]
-pub struct FileRequest {
-    file: String,
-}
-pub struct FileRequestBuilder<T> {
-    file: T,
-}
-pub struct FileNotSet;
-
-impl FileRequest {
-    pub fn new() -> FileRequestBuilder<FileNotSet> {
-        FileRequestBuilder { file: FileNotSet }
-    }
-}
-
-impl FileRequestBuilder<FileNotSet> {
-    pub fn file<U: AsRef<Path>>(self, file: U) -> FileRequestBuilder<U> {
-        FileRequestBuilder { file }
-    }
-}
-
-impl FileRequestBuilder<String> {
-    pub fn build(self) -> FileRequest {
-        FileRequest { file: self.file }
-    }
-}
+pub struct FileRequest;
