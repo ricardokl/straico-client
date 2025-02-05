2dc4fb6f9c8487b7e5e77e91f0f2bc91c1af317e
Ricardo Kullock <ricardokullock@gmail.com>
2025-02-05 15:28:09 +0100

    feat: add list_agents module

--- src/endpoints/agent.rs
+++ src/endpoints/agent.rs
@@ -1,6 +1,7 @@
 pub mod agent_details;
 pub mod completion;
 pub mod create;
+pub mod list_agents;
 pub mod rag_to_agent;
 
 // Delete Agents does not require implementation since it responds just a message
