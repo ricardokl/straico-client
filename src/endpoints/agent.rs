17a5f9a91fa11da7f0991ec3a75bf17dd5a93571
Ricardo Kullock <ricardokullock@gmail.com>
2025-02-05 16:01:08 +0100

    refactor: update agent endpoint

--- src/endpoints/agent.rs
+++ src/endpoints/agent.rs
@@ -5,3 +5,4 @@ pub mod list_agents;
 pub mod rag_to_agent;
 
 // Delete Agents does not require implementation since it responds just a message
+// Update Agent just uses the same requests fields as Create Agent, and the same response as Agent Details
